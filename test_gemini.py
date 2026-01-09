1bbc1b8f0c152b846337b64566ca3ec5ef86f0ed
Sruthi <sruthijayesh427@gmail.com>
2026-01-09 13:08:18 +0530

    rag

--- test_gemini.py
+++ test_gemini.py
@@ -6,6 +6,7 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 llm = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash",
     temperature=0
+    api_key = AIzaSyAHWsYsdTQI0W6OZLBYO-JdlnCIlj2m06g
 )
 
 response = llm.invoke("Say Gemini is working")
